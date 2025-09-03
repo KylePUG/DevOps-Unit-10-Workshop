fe9c4c65ad0dc0112fbb8d02499ecc498e74bc2b
Kyle Liew <kyle.liew@softwire.com>
2025-09-03 14:30:05 +0100

    payload

--- scheduled_jobs.py
+++ scheduled_jobs.py
@@ -35,6 +35,7 @@ def process_orders(app):
         )
 
         app.logger.info("Response from endpoint: " + response.text)
+        app.logger.info(payload)
         response.raise_for_status()
 
         order.set_as_processed()
